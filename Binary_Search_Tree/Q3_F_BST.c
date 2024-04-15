8db5a6dc2810bb97fc1e5e018d8a6d8a62417d10
wooseong-choi <ghkdgo8686@gmail.com>
2024-04-15 19:21:20 +0900

    BST 3번 풀었는데 출력만 맞고 스택엔 다르게 쌓이기 때문에 맞은게 아니다. 근데 더는 못풀겠다.

--- Binary_Search_Tree/Q3_F_BST.c
+++ Binary_Search_Tree/Q3_F_BST.c
@@ -91,7 +91,30 @@ int main()
 
 void preOrderIterative(BSTNode *root)
 {
-	 /* add your code here */
+	Stack stk;
+	stk.top = NULL;
+	
+	while(1){
+		
+		while (root != NULL)
+		{
+			push(&stk,root);
+			printf("\n%d", root->item);
+			root = root->left;
+		}
+
+		if(!isEmpty(&stk)){
+			root = pop(&stk);
+			root = root->right;
+		}else{
+			break;
+		}
+		
+		
+
+	}
+
+
 }
 
 ///////////////////////////////////////////////////////////////////////////////

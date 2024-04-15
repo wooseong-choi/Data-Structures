fd9eda9acb5da1731e7b8d9edfe2d300ef337c6b
wooseong-choi <ghkdgo8686@gmail.com>
2024-04-15 13:48:12 +0900

    이진트리 6번 문제 완료 전위 중위 후위 순회 재귀에 대한 깊은 이해 필요

--- Binary_Tree/Q6_E_BT.c
+++ Binary_Tree/Q6_E_BT.c
@@ -105,7 +105,10 @@ int main()
 
 void printSmallerValues(BTNode *node, int m)
 {
-	/* add your code here */
+    if (node == NULL) return;
+	if (m > node->item) printf("%d ",node->item);
+    printSmallerValues(node->left,m);
+    printSmallerValues(node->right,m);
 }
 
 //////////////////////////////////////////////////////////////////////////////////

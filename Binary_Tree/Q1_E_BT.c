7a643591b476c9c13e310daddad94715b78c9d8c
wooseong-choi <ghkdgo8686@gmail.com>
2024-04-15 10:45:27 +0900

    이진트리 1번 문제 완료

--- Binary_Tree/Q1_E_BT.c
+++ Binary_Tree/Q1_E_BT.c
@@ -116,7 +116,32 @@ int main()
 int identical(BTNode *tree1, BTNode *tree2)
 
 {
-   /* add your code here */
+    if(tree1 == NULL && tree2 == NULL)
+        return 1;
+    else if (tree1 == NULL)
+        return 0;
+    else if (tree2 == NULL)
+        return 0;
+    
+    if(tree1->item == tree2->item){
+        // return 1;
+    
+        if(identical(tree1->left,tree2->left)){
+            // return 1;
+
+            if(identical(tree1->right,tree2->right)){
+                return 1;
+            }else{
+                return 0;
+            }
+        }else{
+            return 0;
+        }
+
+    }else{
+        return 0;
+    }
+    
 }
 
 /////////////////////////////////////////////////////////////////////////////////
